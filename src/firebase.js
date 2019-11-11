8f0339d9e8902a7465e21c9fbde93db5ec50cb78
maxkram <max.kramer@live.com>
2019-11-11 16:25:50 -0500

    before hosting

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,31 @@
 import firebase from 'firebase/app'
 import 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
+import 'firebase/storage'
 
 const config = {
-    apiKey: "AIzaSyCX78TokV6fUr5dv1DTb9Q0E6D6bl5WIFk",
-    authDomain: "man-city-11cb4.firebaseapp.com",
-    databaseURL: "https://man-city-11cb4.firebaseio.com",
-    projectId: "man-city-11cb4",
-    storageBucket: "man-city-11cb4.appspot.com",
-    messagingSenderId: "339073466906"
-  };
+  apiKey: "AIzaSyCX78TokV6fUr5dv1DTb9Q0E6D6bl5WIFk",
+  authDomain: "man-city-11cb4.firebaseapp.com",
+  databaseURL: "https://man-city-11cb4.firebaseio.com",
+  projectId: "man-city-11cb4",
+  storageBucket: "man-city-11cb4.appspot.com",
+  messagingSenderId: "339073466906"
+};
 
 firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
+const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseTeams = firebaseDB.ref('teams');
+const firebasePlayers = firebaseDB.ref('players');
 
 export {
     firebase,
-    firebaseMatches
+    firebaseMatches,
+    firebasePromotions,
+    firebaseTeams,
+    firebasePlayers,
+    firebaseDB
 }
